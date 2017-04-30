16d14100b1b309b0becf5ed15f87ccca17da8c28
Felipe Garrido <figarrido@uc.cl>
2017-04-30 17:27:58 -0300

    Add Transaction model
    rails g model Transaction amount:integer & rake db:migrate

--- Rafflies/db/schema.rb
+++ Rafflies/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430202205) do
+ActiveRecord::Schema.define(version: 20170430202609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 20170430202205) do
   end
 
   create_table "reactions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer  "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
