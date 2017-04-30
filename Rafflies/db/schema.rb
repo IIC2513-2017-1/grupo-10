88176d5add7e60c988dd9162d31c8fd31cfc36ae
Felipe Garrido <figarrido@uc.cl>
2017-04-30 17:24:48 -0300

    Add Wallet model
    rails g model Wallet amount & rake db:migrate

--- Rafflies/db/schema.rb
+++ Rafflies/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430201805) do
+ActiveRecord::Schema.define(version: 20170430202205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 20170430201805) do
     t.string   "name"
     t.string   "password"
     t.string   "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "wallets", force: :cascade do |t|
+    t.integer  "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
