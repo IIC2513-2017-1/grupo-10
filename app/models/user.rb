28f10fd9444dd77b1f0c3f9f463207c36f2c893b
Felipe Garrido <figarrido@uc.cl>
2017-05-09 19:51:23 -0300

    Modify foreign key organizartor for Raffle model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
 	has_many :numbers
 	has_many :raffle_participating, through: :numbers, source: :raffle
-	has_many :raffles
+	has_many :raffles, foreign_key: 'organizator_id'
 	has_many :reactions
 	has_one :wallet, dependent: :destroy
 end
