cacdf585a6f6e7b26d5434814d8b4031761cbf8c
Felipe Garrido <figarrido@uc.cl>
2017-05-14 17:35:44 -0400

    Add controller and view for Sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by username: params[:username]
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user), notice: 'Successful login'
+    else
+      session[:user_id] = nil
+      redirect_to login_path, flash: { error: 'Username or password is incorrect' }
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to raffles_path, notice: 'Successful logout'
   end
 end
