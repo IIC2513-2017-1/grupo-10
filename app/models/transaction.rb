21c2299e20e637e4c00c15083761e87857d57e22
Felipe Garrido <figarrido@uc.cl>
2017-05-14 14:10:00 -0400

    Remove all about wallets

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,31 +7,31 @@ class CustomTransactionValidator < ActiveModel::Validator
       record.errors[:amount].push('A transaction amount must be greater than 0')
     end
     # A transaction can't be made between the same acount
-    return unless record.from_wallet == record.to_wallet
-    record.errors[:to_wallet].push(
+    return unless record.from_user == record.to_user
+    record.errors[:to_user].push(
       "Transaction can't be made between the same account"
     )
-    record.errors[:from_wallet].push(
+    record.errors[:from_user].push(
       "Transaction can't be made between the same account"
     )
   end
 end
 
 class Transaction < ApplicationRecord
-  belongs_to :from_wallet, foreign_key: 'wallet_id', class_name: 'Wallet'
-  belongs_to :to_wallet, foreign_key: 'wallet_id', class_name: 'Wallet'
+  belongs_to :from_user, foreign_key: 'user_id', class_name: 'User'
+  belongs_to :to_user, foreign_key: 'user_id', class_name: 'User'
 
   validates :amount,
             presence: true,
             numericality: true,
             exclusion: { in: [nil] }
-  validates :from_wallet,
+  validates :from_user,
             presence: true,
             exclusion: { in: [nil] }
-  validates :to_wallet,
+  validates :to_user,
             presence: true,
             exclusion: { in: [nil] }
-  validates_associated :from_wallet
-  validates_associated :to_wallet
+  validates_associated :from_user
+  validates_associated :to_user
   validates_with CustomTransactionValidator
 end
