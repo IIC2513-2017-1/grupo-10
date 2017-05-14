728e6697ca1d01e4da73cc2df4ad4b216c703c48
Felipe Garrido <figarrido@uc.cl>
2017-05-14 15:20:28 -0400

    Add validations to Requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+class ApprovedFalseValidator < ActiveModel::Validator
+  def validate(record)
+    return if record.approved
+    record.errors[:approved].push('Approved must be false on creation')
+  end
+end
+
+class CustomRequestValidator < ActiveModel::Validator
+  def validate(record)
+    # Amount must be positive and greater than 0
+    return unless record.amount.positive?
+    record.errors[:amount].push('Requested amount must be greater than 0')
+  end
+end
+
 class Request < ApplicationRecord
   belongs_to :user
 
@@ -11,4 +26,6 @@ class Request < ApplicationRecord
             numericality: true,
             exclusion: { in: [nil] }
   validates_associated :user
+  validates_with CustomRequestValidator
+  validates_with ApprovedFalseValidator, on: :create
 end
