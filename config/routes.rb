6d65431a2575a49ac4ca704c46756eaf6377d207
Felipe Garrido <figarrido@uc.cl>
2017-05-14 16:12:59 -0400

    Add Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get '/login', to: 'sessions#new', as: :login
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy', as: :logout
+
   resources :raffles do
 		resources :numbers, only: [:new, :create, :destroy]
   end
