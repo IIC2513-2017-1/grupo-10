87ea0870e68ef7a4063460e13209d14d8cdcde02
Antonio Ossa <aaossa@uc.cl>
2017-05-13 23:54:24 -0300

    Rubocop says we are cool
    Signed-off-by: Antonio Ossa <aaossa@uc.cl>

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,40 +1,52 @@
+# frozen_string_literal: true
+
 class NumbersController < ApplicationController
   def new
-		@users = User.all
-		@raffle = Raffle.find params[:raffle_id]
-		@number = Number.new
+    @users = User.all
+    @raffle = Raffle.find params[:raffle_id]
+    @number = Number.new
   end
 
+  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
   def create
-		@raffle = Raffle.find params[:raffle_id]
-		number = @raffle.numbers.build number_params
+    @raffle = Raffle.find params[:raffle_id]
+    number = @raffle.numbers.build number_params
 
-		respond_to do |format|
-			if number.save
-				format.html { redirect_to raffle_path(@raffle), notice: 'Number was successfully created.' }
-				format.json { render :show, status: :created, location: @raffle }
-			else
-				format.html { render :new }
-				format.json { render json: number.errors, status: :unprocessable_entity }
-			end
-		end
+    respond_to do |format|
+      if number.save
+        format.html do
+          redirect_to raffle_path(@raffle),
+                      notice: 'Number was successfully created.'
+        end
+        format.json { render :show, status: :created, location: @raffle }
+      else
+        format.html { render :new }
+        format.json do
+          render json: number.errors,
+                 status: :unprocessable_entity
+        end
+      end
+    end
   end
 
-	def destroy
+  def destroy
     number = Number.find(params[:id])
-		raffle = number.raffle
-		number.destroy
+    raffle = number.raffle
+    number.destroy
     respond_to do |format|
-      format.html { redirect_to raffle_path(raffle), notice: 'Raffle was successfully destroyed.' }
+      format.html do
+        redirect_to raffle_path(raffle),
+                    notice: 'Raffle was successfully destroyed.'
+      end
       format.json { head :no_content }
     end
   end
 
-	private
+  private
 
-	def number_params
-		number_param = params.require(:number).permit(:user, :number_in_raffle)
-		number_param[:user] = User.find number_param[:user].to_i
-		number_param
-	end
+  def number_params
+    number_param = params.require(:number).permit(:user, :number_in_raffle)
+    number_param[:user] = User.find number_param[:user].to_i
+    number_param
+  end
 end
