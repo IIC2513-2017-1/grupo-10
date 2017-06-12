3a6c8da2ff32e3781aed6a59cd7d184f49eeaa78
Felipe Garrido <figarrido@uc.cl>
2017-06-11 22:28:54 -0400

    Users can react to raffles and see reaction summary

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -6,25 +6,16 @@ class ReactionsController < ApplicationController
     @before_reaction = Reaction.find_by(user_id: params[:user_id],
                                         raffle_id: params[:raffle_id])
     if @before_reaction
-      p @before_reaction.reaction_representation_id
-      p params[:reaction_representation_id]
       unless @before_reaction.reaction_representation_id ==
              params[:reaction_representation_id].to_i
-        @reaction = Reaction.new reaction_params
-        if @reaction.save
-          respond_to do |format|
-            format.js { render layout: false, action: 'replace' }
-          end
-        end
+        @reaction = Reaction.create reaction_params
       end
       @before_reaction.destroy
     else
-      @reaction = Reaction.new reaction_params
-      if @reaction.save
-        respond_to do |format|
-          format.js { render layout: false, action: 'create' }
-        end
-      end
+      @reaction = Reaction.create reaction_params
+    end
+    respond_to do |format|
+      format.js { render layout: false, action: 'react' }
     end
   end
 
